5317bfb27736b79168dcfe444c3a6e731d0ed722
Jairo Trajano Junior <jairotjunior.edu@gmail.com>
2023-07-24 10:33:09 -0300

    Projeto finalizado

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,19 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles (){
     return gulp.src('./styles/*.scss')
         .pipe(sass({ outputStyle: 'compressed'}))
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('./dist/styles'));
+}
+function images (){
+    return gulp.src('./images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = styles;
+exports.default = gulp.parallel(styles, images);
 exports.watch = function(){
     gulp.watch('./styles/*.scss', gulp.parallel(styles));
 }
